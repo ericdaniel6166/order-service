27ad88752c44bc06732fcabe6f77aee6a6464640
eric6166 <ericnguyen6166@gmail.com>
2023-08-17 22:23:55 +0700

    feature/SM-0005

---
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return orderService.placeOrder(orderRequest);
+    }
+
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("Cannot Place Order Executing Fallback logic");
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
+    }
+}
---
+++ src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,47 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderLineItemsDto;
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderLineItems;
+import com.example.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public String placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+        return "Order Placed";
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
+
