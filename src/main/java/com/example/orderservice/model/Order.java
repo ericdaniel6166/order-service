e8b5e290a390921bc06566c4b191c7890fd0fec3
eric6166 <ericnguyen6166@gmail.com>
2023-08-28 23:04:49 +0700

    feature/SM-0012

--- src/main/java/com/example/orderservice/model/Order.java
+++ src/main/java/com/example/orderservice/model/Order.java
@@ -1,5 +1,6 @@
 package com.example.orderservice.model;
 
+import com.example.springbootmicroservicesframework.model.BaseEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,23 +9,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 
+@Table(name = "order")
 @Entity
-@Table(name = "t_orders")
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Order {
+@EqualsAndHashCode(callSuper = false)
+public class Order extends BaseEntity<String> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 4078865936629506840L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String orderNumber;
     @OneToMany(cascade = CascadeType.ALL)
-    private List<OrderLineItems> orderLineItemsList;
+    private List<OrderLineItem> orderLineItemList;
 }
