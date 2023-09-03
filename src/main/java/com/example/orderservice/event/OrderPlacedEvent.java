f355127fcb279008517abf05d5d7dbe52dd06eb2
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2023-09-03 10:20:21 +0700

    feature/SM-0021 (#12)

---
+++ src/main/java/com/example/orderservice/event/OrderPlacedEvent.java
@@ -0,0 +1,19 @@
+package com.example.orderservice.event;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class OrderPlacedEvent implements Serializable {
+    String orderNumber;
+}
