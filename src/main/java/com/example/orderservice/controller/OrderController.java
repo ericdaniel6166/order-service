ed8d593997a12048d651389a236ab35a5b481c61
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2023-09-24 21:23:13 +0700

    feature/SM-0023 (#14)

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.enums.TypeReq;
 import com.example.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -8,6 +9,7 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,19 +26,39 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    @TimeLimiter(name = "inventory")
-    @Retry(name = "inventory")
-    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
-        log.info("Placing Order");
-        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+//    @TimeLimiter(name = "inventory")
+//    @Retry(name = "inventory")
+//    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+//        log.info("Placing Order");
+//        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+//    }
+//
+//
+//    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+//        log.info("Cannot Place Order, Executing Fallback logic");
+//        return CompletableFuture.supplyAsync(() -> "Something went wrong, please order after some time!");
+//    }
+
+    @GetMapping
+    public String sampleOrder() {
+        log.info("Sample order");
+        return "Sample Order";
     }
 
+    @PostMapping("/web-client")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrderWebClient(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return orderService.placeOrder(orderRequest, TypeReq.WEB_CLIENT);
+    }
 
-    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
-        log.info("Cannot Place Order, Executing Fallback logic");
-        return CompletableFuture.supplyAsync(() -> "Something went wrong, please order after some time!");
+    @PostMapping("/feign-client")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrderFeign(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return orderService.placeOrder(orderRequest, TypeReq.FEIGN_CLIENT);
     }
 }
--- src/main/java/com/example/orderservice/service/OrderService.java
+++ src/main/java/com/example/orderservice/service/OrderService.java
@@ -1,25 +1,27 @@
 package com.example.orderservice.service;
 
+import com.example.orderservice.client.InventoryClient;
 import com.example.orderservice.dto.InventoryResponse;
 import com.example.orderservice.dto.OrderLineItemsDto;
 import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.enums.TypeReq;
 import com.example.orderservice.event.OrderPlacedEvent;
+import com.example.orderservice.kafka.KafkaProducerProperties;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.model.OrderLineItem;
+import com.example.springbootmicroservicesframework.kafka.Event;
 import com.example.orderservice.repository.OrderRepository;
-import com.example.springbootmicroservicesframework.kafka.AppEvent;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.cloud.sleuth.Span;
-import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -33,13 +35,17 @@ public class OrderService {
 
     final OrderRepository orderRepository;
 
-    final WebClient.Builder webClient;
-
-    final Tracer tracer;
-
+    final WebClient.Builder webClientBuilder;
+    //
+//    final Tracer tracer;
+//
     final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public String placeOrder(OrderRequest orderRequest) {
+    final InventoryClient inventoryClient;
+
+    final KafkaProducerProperties kafkaProducerProperties;
+
+    public String placeOrder(OrderRequest orderRequest, TypeReq typeReq) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItem> orderLineItems = orderRequest.getOrderLineItemsDtoList()
@@ -50,35 +56,74 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemList().stream()
                 .map(OrderLineItem::getSkuCode)
                 .toList();
-        Span span = tracer.nextSpan().name("inventory-service-lookup");
-        try (Tracer.SpanInScope ignored = tracer.withSpan(span.start())) {
-            InventoryResponse[] inventoryResponseArray = webClient.build().get()
-                    .uri("http://inventory-service/api/inventory",
-                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                    .retrieve()
-                    .bodyToMono(InventoryResponse[].class)
-                    .block();
-
-            boolean allProductsInStock = false;
-            if (ObjectUtils.isNotEmpty(inventoryResponseArray)) {
-                allProductsInStock = Arrays.stream(inventoryResponseArray)
-                        .allMatch(InventoryResponse::isInStock);
+        List<InventoryResponse> inventoryResponseList = new ArrayList<>();
+        switch (typeReq) {
+            case WEB_CLIENT -> inventoryResponseList = Arrays.stream(webClientBuilder.build().get()
+                            .uri("http://inventory-service/api/inventory",
+                                    uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                            .retrieve()
+                            .bodyToMono(InventoryResponse[].class)
+                            .block())
+                    .toList();
+            case FEIGN_CLIENT -> inventoryResponseList = inventoryClient.searchInventory(skuCodes);
+            default -> {
             }
-            if (Boolean.TRUE.equals(allProductsInStock)) {
-                orderRepository.saveAndFlush(order);
-                kafkaTemplate.send("notificationTopic", AppEvent.builder()
-                        .payload(OrderPlacedEvent.builder()
-                                .orderNumber(order.getOrderNumber())
-                                .build())
-                        .build());
-                return "Order Placed";
-            } else {
-                throw new IllegalArgumentException("Product is not in stock, please try again later");
-            }
-        } finally {
-            span.end();
         }
+        boolean allProductsInStock = false;
+        if (ObjectUtils.isNotEmpty(inventoryResponseList)) {
+            allProductsInStock = inventoryResponseList.stream()
+                    .allMatch(InventoryResponse::isInStock);
+        }
+        if (Boolean.TRUE.equals(allProductsInStock)) {
+            orderRepository.saveAndFlush(order);
+            kafkaTemplate.send(kafkaProducerProperties.getNotificationTopicName(), Event.builder()
+                    .payload(OrderPlacedEvent.builder()
+                            .orderNumber(order.getOrderNumber())
+                            .build())
+                    .build());
+            kafkaTemplate.send(kafkaProducerProperties.getInternalTopicName(), Event.builder()
+                    .payload(OrderPlacedEvent.builder()
+                            .orderNumber(order.getOrderNumber())
+                            .build())
+                    .build());
+            log.info("inventoryResponseList : {}", inventoryResponseList);
+            return "Order Placed";
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
+
+
+
+//        Span span = tracer.nextSpan().name("inventory-service-lookup");
+//        try (Tracer.SpanInScope ignored = tracer.withSpan(span.start())) {
+//            InventoryResponse[] inventoryResponseArray = webClient.build().get()
+//                    .uri("http://inventory-service/api/inventory",
+//                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+//                    .retrieve()
+//                    .bodyToMono(InventoryResponse[].class)
+//                    .block();
+
+//            boolean allProductsInStock = false;
+//            if (ObjectUtils.isNotEmpty(inventoryResponseArray)) {
+//                allProductsInStock = Arrays.stream(inventoryResponseArray)
+//                        .allMatch(InventoryResponse::isInStock);
+//            }
+//            if (Boolean.TRUE.equals(allProductsInStock)) {
+//                orderRepository.saveAndFlush(order);
+//                kafkaTemplate.send("notificationTopic", AppEvent.builder()
+//                        .payload(OrderPlacedEvent.builder()
+//                                .orderNumber(order.getOrderNumber())
+//                                .build())
+//                        .build());
+//                return "Order Placed";
+//            } else {
+//                throw new IllegalArgumentException("Product is not in stock, please try again later");
+//            }
+//        } finally {
+//            span.end();
+//        }
     }
+
 
     private OrderLineItem mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItem orderLineItem = new OrderLineItem();
@@ -87,5 +132,6 @@ public class OrderService {
         orderLineItem.setSkuCode(orderLineItemsDto.getSkuCode());
         return orderLineItem;
     }
+
 }
 
