77c38ed9c5b60d0c3f91309440b11a5c7893f2bd
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2023-10-17 01:00:05 +0700

    Feature/sm 0032 (#22)
    * feature/SM-0032
    
    * feature/SM-0032

--- src/main/java/com/example/orderservice/integration/client/InventoryClient.java
+++ src/main/java/com/example/orderservice/integration/client/InventoryClient.java
@@ -1,11 +1,8 @@
 package com.example.orderservice.integration.client;
 
-import com.example.springbootmicroservicesframework.config.feign.FeignClientConfig;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "${feign.client.config.inventory-client.name}",
-        url = "${feign.client.config.inventory-client.url}",
-        configuration = FeignClientConfig.class)
-public interface InventoryClient {
-
-}
+//@FeignClient(name = "${feign.client.config.inventory-client.name}",
+//        url = "${feign.client.config.inventory-client.url}",
+//        configuration = FeignClientConfig.class)
+//public interface InventoryClient {
+//
+//}
