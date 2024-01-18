48bdf1cb7c18612009eccf58bc2b8a73acdffce1
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2024-01-19 00:43:05 +0700

    feature/SM-0040 (#31)

--- src/main/java/com/example/orderservice/dto/OrderStatusResponse.java
+++ src/main/java/com/example/orderservice/dto/OrderStatusResponse.java
@@ -7,12 +7,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder
-public class OrderStatusResponse {
+public class OrderStatusResponse implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     Long orderId;
     String orderStatus;
     //    @JsonIgnore //uncomment
--- src/main/java/com/example/orderservice/dto/PlaceOrderResponse.java
+++ src/main/java/com/example/orderservice/dto/PlaceOrderResponse.java
@@ -8,11 +8,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class PlaceOrderResponse {
+public class PlaceOrderResponse implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     OrderStatus orderStatus;
 }
